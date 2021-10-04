c756bcdf17ebfc72b689dab11c090e19b05f5e73
xianhezhang <3600276843@qq.com>
2021-10-04 22:50:23 +0800

    update file format

--- 字节备战/Solution.js
+++ 字节备战/Solution.js
@@ -3,7 +3,17 @@
 // 数组去重
 // Vue书写
 // promise.all
+// promise.any
 // 原型链继承
 // bind/call/apply
 // 深拷贝
 // ！！json数组转换
+//函数add 接受任意数量的数字
+
+// 改代码输出
+var obj = {
+  value : 'obj',
+  fn : function(){ console.log(this.value)}
+}
+var fn = obj.fn;
+fn();

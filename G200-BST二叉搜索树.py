446ec94fb0d3d009e4c73b67d9e452e1a0564db1
xianhezhang <3600276843@qq.com>
2021-11-17 14:42:31 +0800

    11.17update

--- G200-BST二叉搜索树.py
+++ G200-BST二叉搜索树.py
@@ -209,14 +209,212 @@ class Solution {
   }
 }
 
-
 """
-#108
 
-#109
 
-#653
-#530
+#108 将有序数组转换为二叉搜索树
+"""因为二叉树的递归是有顺序，因此非常适合采用分治的思想，首先想要构建树，每一层的书是我们要规定的"""
+class Solution:
+  def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+    return self.dfs(nums, 0, len(nums) - 1)
 
-#501
+  def dfs(self, nums:List[int], low: int, high: int) -> TreeNode:
+    if low > high:
+      return None
+    mid = low + (high - low) // 2
+    root = TreeNode(nums[mid])
+    root.left = self.dfs(nums, low, mid - 1)
+    root.right = self.dfs(nums, mid + 1, high)
+    return root
 
+#109 有序链表转换二叉搜索树
+"""有序链表，我有两个想法：1.同108先转化成有序数组，再构建 2.直接中序遍历构建，但是具体是什么结构的，我没有把握
+我没有想到的方法：3. 快慢指针（找中点的特性）
+"""
+  #转化成有序数组
+class Solution:
+  def sortedListToBST(self, head: ListNode) -> TreeNode:
+    curList = self.linkedNodesToArray(head)
+    return self.dfs(curList, 0, len(curList) - 1)
+
+  def linkedNodesToArray(self, head):
+    res = []
+    while head:
+      res.append(head.val)
+      head = head.next  #head.next = head.next.next
+    return res
+
+  def dfs(self, nums:List[int], low: int, high: int) -> TreeNode:
+    if low > high:
+      return None
+    mid = low + (high - low) // 2
+    root = TreeNode(nums[mid])
+    root.left = self.dfs(nums, low, mid - 1)
+    root.right = self.dfs(nums, mid + 1, high)
+    return root
+#时间复杂度: n + logn = n 空间复杂度n + logn  = n
+
+#快慢指针
+#为什么选择从中点开始构造整个树？因为高度差不能超过一！所以左右两个要比较紧密排列
+class Solution:
+  def sortedListToBST(self, head: ListNode) -> TreeNode:
+    if not head: return None
+    if not head.next: return TreeNode(head.val)
+
+    slow, fast, pre = head, head, head
+    while not fast and not fast.next:
+      pre = slow 
+      slow = slow.next
+      fast = fast.next.next
+    #理想情况下，slow是中点
+    right = slow.next
+    pre.next = None
+    root = TreeNode(slow.val)
+    root.left = self.sortedListToBST(head)
+    root.right = self.sortedListToBST(right)
+    return root
+
+#复杂度 时间复杂度nlogn n是每一次递归的时候都熬while遍历 logn是栈 空间复杂度logn 调用栈
+
+#方法三：中序遍历优化 仍需要用到len()求中点mid
+class Solution:
+  def sortedListToBST(self, head: ListNode) -> TreeNode:
+    if not head: return None
+    length = 0
+    self.ptr = head#不能放在下面，因为下面的head已经不是最开始的head了
+    while head:
+      length += 1
+      head = head.next
+    # self.ptr = head #这里不能放在这里
+    return self.buildTree(0, length - 1)
+
+  def buildTree(self, start, end):
+    if start > end: return None
+    #这里的mid不做切断链条的处理，而是用于标记
+    mid = start + (end - start) // 2
+    leftTree = self.buildTree(start, mid - 1)
+    root = TreeNode(self.ptr.val)   #这里是访问不到self.ptr的，self可以传递给子方法。
+    #更新链表标记
+    s  elf.ptr = self.ptr.next 
+    #连接左子树
+    root.left = leftTree
+    root.right = self.buildTree(mid + 1, end)
+    return root
+
+
+
+#653（这题好垃圾，没用到bst的feature呀）两数之和 IV - 输入 BST
+#写一种解法吧：新建set，用来存储已经遍历过的值，然后去遍历，如果有值，就返回true否则false
+class Solution:
+  def findTarget(self, root: TreeNode, k: int) -> bool:
+    if not root:
+      return False
+    visited = [root]
+    targetList = set()
+    while visited:
+      cur = visited.pop(0)
+      if cur.val in targetList:
+        return True
+      targetList.add(k - cur.val)
+      if cur.left:
+        visited.append(cur.left)
+      if cur.right:
+        visited.append(cur.right)
+    
+    return False
+#嘿嘿自己写的，97%！
+#复杂度时间空间都为n
+
+
+#530 二叉搜索树的最小绝对差
+#这题：中序遍历有序数组 + 找相邻的差值
+class Solution:
+  def getMinimumDifference(self, root: TreeNode) -> int:
+    def dfs(root):
+      if not root:
+        return
+      dfs(root.left)
+      queue.append(root.val)
+      dfs(root.right)
+
+
+    queue = []
+    target = float("inf")
+    dfs(root)
+    for i in range(0,len(queue) - 1):
+      if queue[i + 1] - queue[i] < target:
+        target = queue[i + 1] - queue[i]
+    return target
+#这一题的range卡住我了。不应该为-2
+#ps.求target的方法，可以直接放在中序遍历中, 这样就不用维护一个list存放遍历的值了
+class Solution:
+  def getMinimumDifference(self, root: TreeNode) -> int:
+    def dfs(root):
+      if not root:
+        return
+      dfs(root.left)
+      if self.pre == -1:
+        self.pre = root.val
+      else:
+        self.target = min(self.target, root.val - self.pre)
+        self.pre = root.val
+      dfs(root.right)
+
+    self.pre = -1
+    self.target = float("inf")
+    dfs(root)
+    return self.target
+#这种速度快非常多！！时间复杂度 n 空间logn
+
+#501二叉搜索树中的众数
+"""Morris 中序遍历
+主要工具是cur和一个指针，先遍历左边的，按照cur找上一级pre，然后通过建立的连接，回溯到最开始的root位置用于开始遍历右子树！
+比较难理解，现在看代码
+"""
+class Solution:
+  def findMode(self, root: TreeNode) -> List[int]:
+    cur = root                                        #初始化cur位置
+    self.ans = []
+    self.count = 0
+    self.maxCount = 0
+    self.base = float("-inf")
+    while cur:                                        #进入循环，开始遍历
+      if cur.left == None:                            #如果左子树没有值，直接进入下一个node，并更新数据。
+        self.update(cur.val)
+        cur = cur.right
+        continue
+      pre = cur.left                                  #cur的左子树不为空时，让pre下去。
+      #去找到cur的pre的末尾位置，注意！这里是结点对比，而非是值的对比✨！
+      while (pre.right and pre.right != cur):         #右子树不为空，并且右子树不为cur时，指针停下（这是结合了BST与中序遍历的特性来的）
+        pre = pre.right
+      #not pre.right同时意味着cur的前续没有相同的
+      if not pre.right:                               #如果遍历到的pre的右子树没有了，意味着此时pre就是cur的前面那个，可以连接起来了！
+        pre.right = cur 
+        cur = cur.left                                #cur往左下放一环，继续重复整个while操作
+      else:
+        pre.right = None                              #如果相同找到与cur相同的值，更新一次update重数，cur向右移
+        self.update(cur.val)
+        cur = cur.right
+
+    return self.ans
+
+  def update(self, x: int):
+    if x == self.base:
+      self.count += 1
+    else:
+      self.count = 1
+      self.base = x
+    if self.count == self.maxCount:
+      self.ans.append(self.base)
+    if self.count > self.maxCount:
+      self.maxCount = self.count
+      # self.ans.pop(0)   这里不能用这个，因为试想我们append时候针对每一个数，只append一次，但是如果count一直增加，那么我们会一直pop出去，pop到后面是empty list报错！
+      self.ans = []
+      self.ans.append(self.base)
+      
+
+
+
+#208
+
+#677 

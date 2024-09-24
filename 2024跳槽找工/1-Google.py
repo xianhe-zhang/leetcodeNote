dcc80bb1d0c54509efa9582e078d1238ef4c93f0
Xianhe (Mario) Zhang <xianhe1004@gmail.com>
2024-09-23 23:18:35 -0400

    recommit

--- 2024跳槽找工/1-Google.py
+++ 2024跳槽找工/1-Google.py
@@ -1762,7 +1762,39 @@ class Solution:
 
         
 # 843
+class Solution(object):
+    def findSecretWord(self, wordlist, master):
+		
+        def pair_matches(a, b):         # count the number of matching characters
+            return sum(c1 == c2 for c1, c2 in zip(a, b))
 
+        def most_overlap_word():
+            counts = [[0 for _ in range(26)] for _ in range(6)]     # counts[i][j] is nb of words with char j at index i
+            for word in candidates:
+                for i, c in enumerate(word):
+                    counts[i][ord(c) - ord("a")] += 1
+
+            best_score = 0
+            for word in candidates:
+                score = 0
+                for i, c in enumerate(word):
+                    score += counts[i][ord(c) - ord("a")]           # all words with same chars in same positions
+                if score > best_score:
+                    best_score = score
+                    best_word = word
+
+            return best_word
+
+        candidates = wordlist[:]        # all remaining candidates, initially all words
+        while candidates:
+
+            s = most_overlap_word()     # guess the word that overlaps with most others
+            matches = master.guess(s)
+
+            if matches == 6:
+                return
+
+            candidates = [w for w in candidates if pair_matches(s, w) == matches]   # filter words with same matches
 # 332
 # 2345
 # 1857
